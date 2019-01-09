eab8c55e30814d56209b2dc5dc24441fddae8643
Allison Howard <alliemhow@gmail.com>
2019-01-09 07:51:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,8 +105,8 @@ class Student
         LIMIT ?
       SQL
 
-      DB[:conn].execute(sql).map do |row|
+      DB[:conn].execute(sql, number).map do |row|
         self.new_from_db(row)
-      end.first
+      end
     end
 end
